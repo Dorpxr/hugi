d3b42a30ce959cf913783f9374cf16f629a96735
Austin Howard <ahoward@austinhoward.tech>
2023-06-13 20:41:53 -0400

    change hero blurb img source to png

--- components/HeroBlurb.tsx
+++ components/HeroBlurb.tsx
@@ -5,7 +5,7 @@ export const HeroBlurb = () => {
   return (
     <div className="flex flex-col py-12 md:flex-row">
       <div className="w-full overflow-hidden rounded-lg bg-neutral-300 p-12 md:w-5/12">
-        <img src="/static/images/hero-blurb.jpg" />
+        <img src="/static/images/hero-blurb.png" />
       </div>
       <div className="w-full pt-6 md:w-7/12 md:pt-0 md:pl-28">
         <h2 className="pb-8 text-2xl font-bold">{heroBlurbText.header}</h2>
